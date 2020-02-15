6fe16a5551317140eee21dc9c64330b2d05fbbe9
Carlos Roman <carlosrmn005@hotmail.com>
2020-02-15 12:16:21 -0800

    Project does what is intended; will add more functionality and apply better visuals in the future

--- driver.py
+++ driver.py
@@ -3,12 +3,13 @@ import sortFunctions
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import random
-from matplotlib.widgets import Button, TextBox
+from matplotlib.widgets import Button, TextBox, RadioButtons
 
 low = 0
 high = 100
 numInArray = 50
 maxNumber = 50
+sortSpeed = 1  # Fast = .01; Medium = .5; Slow = 1
 
 
 class Index:
@@ -32,25 +33,324 @@ class Index:
         # plt.cla()
         fillXYarray(yAxis, xAxis, numInArray, low, high)
         graphSetup()
-        # histButton1 = Button(histAxes1, "Update", color='lightgoldenrodyellow', hovercolor='0.975')
+
+    def bubble(self, event):
+        self.ind += 1
+        sortButton.color = "aliceblue"
+        quickButton.color = "aliceblue"
+        mergeButton.color = "aliceblue"
+        insertButton.color = "aliceblue"
+        bubbleButton.color = "lightblue"
+        selectionButton.color = "aliceblue"
+
+    def select(self, event):
+        self.ind += 1
+        sortButton.color = "aliceblue"
+        quickButton.color = "aliceblue"
+        mergeButton.color = "aliceblue"
+        insertButton.color = "aliceblue"
+        bubbleButton.color = "aliceblue"
+        selectionButton.color = "lightblue"
+
+    def quick(self, event):
+        self.ind += 1
+        sortButton.color = "aliceblue"
+        quickButton.color = "lightblue"
+        mergeButton.color = "aliceblue"
+        insertButton.color = "aliceblue"
+        bubbleButton.color = "aliceblue"
+        selectionButton.color = "aliceblue"
+
+    def merge(self, event):
+        self.ind += 1
+        sortButton.color = "aliceblue"
+        quickButton.color = "aliceblue"
+        mergeButton.color = "lightblue"
+        insertButton.color = "aliceblue"
+        bubbleButton.color = "aliceblue"
+        selectionButton.color = "aliceblue"
+
+    def insert(self, event):
+        self.ind += 1
+        sortButton.color = "aliceblue"
+        quickButton.color = "aliceblue"
+        mergeButton.color = "aliceblue"
+        insertButton.color = "lightblue"
+        bubbleButton.color = "aliceblue"
+        selectionButton.color = "aliceblue"
+
+    def sort(self, event):
+        self.ind += 1
+        sortButton.color = "lightblue"
+
+        if bubbleButton.color == "lightblue":
+            bubbleSort()
+
+        elif selectionButton.color == "lightblue":
+            selectionSort()
+
+        elif insertButton.color == "lightblue":
+            insertionSort()
+        elif mergeButton.color == "lightblue":
+            mergeSort(yAxis, 0, len(yAxis) - 1)
+        elif quickButton.color == "lightblue":
+            quickSort(yAxis, 0, len(yAxis) - 1)
+        sortButton.color = "aliceblue"
 
 
-def graphSetup():
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
-    ax.set_title("Graph")
-    x_axis = ax.axes.get_xaxis()
-    x_label = x_axis.get_label()
-    x_label.set_visible(False)
-    y_axis = ax.axes.get_yaxis()
-    y_label = y_axis.get_label()
-    y_label.set_visible(False)
-    b = ax.bar(xAxis, yAxis, label="Bar1", color="royalblue")
-    # plt.title("Test Graph")
-    # plt.xlabel("x")
-    # plt.ylabel("y")
+def bubbleSort():
+    bubbleButton.color = "aliceblue"
+    n = len(yAxis)  # gets the length of the array
 
-    plt.tick_params(axis="x", which="both", bottom=False, top=False, labelbottom=False)
+    # Go through the whole array
+    for i in range(n):  # range(start, stop, step) -> (optinal, required, optional)
+        # After this for loop the last i elements should be in order
+        for j in range(0, n - i - 1):
+            # time.sleep(1)
+            ax.cla()
+            graphSetup(j, j + 1)
+            plt.pause(sortSpeed)
+            # If current element is greater than next element swap
+            if yAxis[j] > yAxis[j + 1]:
+                yAxis[j], yAxis[j + 1] = yAxis[j + 1], yAxis[j]
+                ax.cla()
+                graphSetup(j, j + 1)
+        ax.cla()
+        graphSetup()
+
+
+def selectionSort():
+    selectionButton.color = "aliceblue"
+    n = len(yAxis)
+
+    for i in range(n):  # go through the array
+        minimum = i
+        for j in range(i + 1, n):  # find the smallest element from the remaining array
+            ax.cla()
+            graphSetup(i, j)
+            plt.pause(sortSpeed)
+            if yAxis[minimum] > yAxis[j]:
+                minimum = j
+
+        yAxis[i], yAxis[minimum] = yAxis[minimum], yAxis[i]  # swap
+    ax.cla()
+    graphSetup()
+
+
+def insertionSort():
+    insertButton.color = "aliceblue"
+    n = len(yAxis)
+
+    for i in range(1, n):
+        key = yAxis[i]
+
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i - 1
+        ax.cla()
+        graphSetup(i)
+        plt.pause(sortSpeed)
+        while j >= 0 and key < yAxis[j]:
+            yAxis[j + 1] = yAxis[j]
+            j -= 1
+        yAxis[j + 1] = key
+        ax.cla()
+        graphSetup(j + 1)
+        plt.pause(sortSpeed)
+
+    ax.cla()
+    graphSetup()
+
+
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+
+    # create temp arrays
+    L = [0] * n1
+    R = [0] * n2
+
+    # Copy data to temp arrays L[] and R[]
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+
+        # Merge the temp arrays back into arr[l..r]
+    i = 0  # Initial index of first subarray
+    j = 0  # Initial index of second subarray
+    k = l  # Initial index of merged subarray
+
+    while i < n1 and j < n2:
+        ax.cla()
+        graphSetup(k)
+        plt.pause(sortSpeed)
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    # Copy the remaining elements of L[], if there
+    # are any
+    while i < n1:
+        ax.cla()
+        graphSetup(k)
+        plt.pause(sortSpeed)
+        arr[k] = L[i]
+        i += 1
+        k += 1
+        ax.cla()
+        graphSetup(k)
+        plt.pause(sortSpeed)
+
+    # Copy the remaining elements of R[], if there
+    # are any
+    while j < n2:
+        ax.cla()
+        graphSetup(k)
+        plt.pause(sortSpeed)
+        arr[k] = R[j]
+        j += 1
+        k += 1
+        ax.cla()
+        graphSetup(k)
+        plt.pause(sortSpeed)
+
+
+# l is for left index and r is right index of the
+# sub-array of arr to be sorted
+def mergeSort(arr, l, r):
+    mergeButton.color = "aliceblue"
+    if l < r:
+        # Same as (l+r)/2, but avoids overflow for
+        # large l and h
+        m = (l + (r - 1)) // 2
+        m_int = int(m)
+        # Sort first and second halves
+        mergeSort(arr, l, m_int)
+        mergeSort(arr, m_int + 1, r)
+        merge(arr, l, m_int, r)
+        ax.cla()
+        graphSetup()
+        plt.pause(sortSpeed)
+
+
+def partition(arr, low, high):
+    i = (low - 1)
+    pivot = arr[high]
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i = i + 1
+            ax.cla()
+            graphSetup(i, high, j)
+            plt.pause(sortSpeed)
+            arr[i], arr[j] = arr[j], arr[i]
+            ax.cla()
+            graphSetup(i, high, j)
+            plt.pause(sortSpeed)
+
+    ax.cla()
+    graphSetup(i + 1, high)
+    plt.pause(sortSpeed)
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    ax.cla()
+    graphSetup(i + 1, high)
+    plt.pause(sortSpeed)
+    return i + 1
+
+
+def quickSort(arr, low, high):
+    quickButton.color = "aliceblue"
+    if low < high:
+        par = partition(arr, low, high)
+
+        quickSort(arr, low, par - 1)
+        quickSort(arr, par + 1, high)
+    ax.cla()
+    graphSetup()
+    plt.pause(sortSpeed)
+
+
+def graphSetup(*args):
+    if len(args) == 0:
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.set_title("Graph")
+        x_axis = ax.axes.get_xaxis()
+        x_label = x_axis.get_label()
+        x_label.set_visible(False)
+        y_axis = ax.axes.get_yaxis()
+        y_label = y_axis.get_label()
+        y_label.set_visible(False)
+        # plt.title("Test Graph")
+        # plt.xlabel("x")
+        # plt.ylabel("y")
+        b = ax.bar(xAxis, yAxis, label="Bar1", color="royalblue")
+        plt.tick_params(axis="x", which="both", bottom=False, top=False, labelbottom=False)
+
+    elif len(args) == 1:
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.set_title("Graph")
+        x_axis = ax.axes.get_xaxis()
+        x_label = x_axis.get_label()
+        x_label.set_visible(False)
+        y_axis = ax.axes.get_yaxis()
+        y_label = y_axis.get_label()
+        y_label.set_visible(False)
+        # plt.title("Test Graph")
+        # plt.xlabel("x")
+        # plt.ylabel("y")
+        b = ax.bar(xAxis, yAxis, label="Bar1", color="royalblue")
+        if args[0] >= len(yAxis):
+            b[len(yAxis) - 1].set_color("orangered")
+        else:
+            b[args[0]].set_color("orangered")
+        plt.tick_params(axis="x", which="both", bottom=False, top=False, labelbottom=False)
+
+    elif len(args) == 2:
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.set_title("Graph")
+        x_axis = ax.axes.get_xaxis()
+        x_label = x_axis.get_label()
+        x_label.set_visible(False)
+        y_axis = ax.axes.get_yaxis()
+        y_label = y_axis.get_label()
+        y_label.set_visible(False)
+        # plt.title("Test Graph")
+        # plt.xlabel("x")
+        # plt.ylabel("y")
+        b = ax.bar(xAxis, yAxis, label="Bar1", color="royalblue")
+        b[args[0]].set_color("orangered")
+        b[args[1]].set_color("peachpuff")
+        plt.tick_params(axis="x", which="both", bottom=False, top=False, labelbottom=False)
+
+    elif len(args) == 3:
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.set_title("Graph")
+        x_axis = ax.axes.get_xaxis()
+        x_label = x_axis.get_label()
+        x_label.set_visible(False)
+        y_axis = ax.axes.get_yaxis()
+        y_label = y_axis.get_label()
+        y_label.set_visible(False)
+        # plt.title("Test Graph")
+        # plt.xlabel("x")
+        # plt.ylabel("y")
+        b = ax.bar(xAxis, yAxis, label="Bar1", color="royalblue")
+        b[args[0]].set_color("orangered")
+        b[args[1]].set_color("peachpuff")
+        b[args[2]].set_color("r")
+        plt.tick_params(axis="x", which="both", bottom=False, top=False, labelbottom=False)
+
 
 # There is an easier way to do this but Im a noob so this is my solution-------------------
 # to this----------------------------------------------------------------------------------
@@ -97,7 +397,8 @@ fig = plt.figure()
 fig.canvas.set_window_title("Sorting Visualizer")
 
 # Label stating "Choose sort method"--------------------------------------------------------
-ax = plt.axes([.20, .25, .6, .6])
+ax = plt.axes([.25, .25, .6, .6])
+b = ax.bar(xAxis, yAxis, label="Bar1", color="royalblue")
 labelAxes = plt.axes([0.65, 0.08, 0.09, 0.05])
 sortLabel()
 # Making the Buttons and Text Area----------------------------------------------------------
@@ -119,15 +420,37 @@ quickButton = Button(quickAxes, "Quick", color="aliceblue", hovercolor="lightblu
 sortButton = Button(sortAxes, "SORT", color="aliceblue", hovercolor="lightblue")
 text_box = TextBox(textBoxAxes, "Type Here ", initial="", color="w")
 updateButton.on_clicked(callback.update)
-bubbleButton.on_clicked(callback.update)
-selectionButton.on_clicked(callback.update)
-insertButton.on_clicked(callback.update)
-mergeButton.on_clicked(callback.update)
-quickButton.on_clicked(callback.update)
-sortButton.on_clicked(callback.update)
+bubbleButton.on_clicked(callback.bubble)
+selectionButton.on_clicked(callback.select)
+insertButton.on_clicked(callback.insert)
+mergeButton.on_clicked(callback.merge)
+quickButton.on_clicked(callback.quick)
+sortButton.on_clicked(callback.sort)
 text_box.on_submit(submit)
 
+# Making Radio Buttons--------------------------------------------------------------------
+rax = plt.axes([0.39, 0.01, 0.1, 0.15], facecolor="w")
+rax.spines["top"].set_visible(False)
+rax.spines["right"].set_visible(False)
+rax.spines["bottom"].set_visible(False)
+rax.spines["left"].set_visible(False)
+radio = RadioButtons(rax, ("Slow", "Medium", "Fast"))
+for circle in radio.circles:
+    circle.set_radius(0.05)
 
+
+def speedFunction(label):
+    global sortSpeed
+    if label == "Slow":
+        sortSpeed = 1
+    elif label == "Medium":
+        sortSpeed = .5
+    elif label == "Fast":
+        sortSpeed = .01
+    plt.draw()
+
+
+radio.on_clicked(speedFunction)
 graphSetup()
 plt.show()
 
--- sortFunctions.py
+++ sortFunctions.py
@@ -26,122 +26,174 @@ def partition(arr, low, high):
     return i + 1
 
 
-class sortFunc:
-    # Bubble Sort----------------------------------------------------------------------------
-    def bubbleSort(self, arr):
-        n = len(arr)  # gets the length of the array
+# Bubble Sort----------------------------------------------------------------------------
+def bubbleSort(arr):
+    n = len(arr)  # gets the length of the array
 
-        # Go through the whole array
-        for i in range(n):  # range(start, stop, step) -> (optinal, required, optional)
-
-            # After this for loop the last i elements should be in order
-            for j in range(0, n - i - 1):
-                # If current element is greater than next element swap
-                if arr[j] > arr[j + 1]:
-                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
-    # Selection Sort----------------------------------------------------------------------------
-    def selectionSort(self, arr):
-        n = len(arr)
-
-        for i in range(n):  # go through the array
-            min = i
-            for j in range(i + 1, n):  # find the smallest element from the remaining array
-                if arr[min] > arr[j]:
-                    min = j
-
-            arr[i], arr[min] = arr[min], arr[i]  # swap
-
-    # Insertion Sort----------------------------------------------------------------------------
-    def insertionSort(self, arr):
-        n = len(arr)
-
-        for i in range(1, n):
-            key = arr[i]
-
-            # Move elements of arr[0..i-1], that are
-            # greater than key, to one position ahead
-            # of their current position
-            j = i - 1
-            while j >= 0 and key < arr[j]:
-                arr[j + 1] = arr[j]
-                j -= 1
-            arr[j + 1] = key
-
-    # Merge Sort----------------------------------------------------------------------------
-    def mergeSort(self, arr):
-        n = len(arr)
-        if n > 1:
-            mid = n // 2        #need // for floor division, using / will give error
-            left = arr[:mid]    #:mid returns everything before mid including mid
-            right = arr[mid:]   # mid: returns everything after mid
-
-            self.mergeSort(left)
-            self.mergeSort(right)
-
-            i = j = k = 0
-
-            while i < len(left) and j < len(right):
-                if left[i] < right[j]:
-                    arr[k] = left[i]
-                    i += 1
-                else:
-                    arr[k] = right[j]
-                    j += 1
-                k += 1
-
-            while i < len(left):
-                arr[k] = left[i]
-                i += 1
-                k += 1
-
-            while j < len(right):
-                arr[k] = right[j]
-                j += 1
-                k += 1
-
-    # Quick Sort----------------------------------------------------------------------------
-
-    def quickSort(self, arr, low, high):
-        if low < high:
-            par = partition(arr, low, high)
-
-            self.quickSort(arr, low, par - 1)
-            self.quickSort(arr, par + 1, high)
+    # Go through the whole array
+    for i in range(n):  # range(start, stop, step) -> (optinal, required, optional)
+        # After this for loop the last i elements should be in order
+        for j in range(0, n - i - 1):
+            # If current element is greater than next element swap
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
-arr1 = [10, 7, 8, 9, 1, 5]
-n1 = len(arr1)
-sf = sortFunc()
-sf.quickSort(arr1, 0, n1 - 1)
-print("Sorted array by Quick Sort is:")
-for i in range(n1):
-    print(arr1[i], end=" ")
+# Selection Sort----------------------------------------------------------------------------
+def selectionSort(arr):
+    n = len(arr)
 
-arr2 = [4, 10, 9, 3, 6, 1]
-n2 = len(arr2)
-sf.bubbleSort(arr2)
-print("\nSorted array by Bubble Sort is:")
-for i in range(n2):
-    print(arr2[i], end=" ")
+    for i in range(n):  # go through the array
+        min = i
+        for j in range(i + 1, n):  # find the smallest element from the remaining array
+            if arr[min] > arr[j]:
+                min = j
 
-arr3 = [10, 6, 7, 5, 9, 8]
-n3 = len(arr3)
-sf.insertionSort(arr3)
-print("\nSorted array by Insertion Sort is:")
-for i in range(n3):
-    print(arr3[i], end=" ")
+        arr[i], arr[min] = arr[min], arr[i]  # swap
 
-arr4 = [5, 2, 4, 6, 8, 2]
-n4 = len(arr4)
-sf.mergeSort(arr4)
-print("\nSorted array by Merge Sort is:")
-for i in range(n4):
-    print(arr3[i], end=" ")
 
-arr5 = [5, 10, 7, 5, 6, 1]
-n5 = len(arr5)
-sf.selectionSort(arr5)
-print("\nSorted array by Selection Sort is:")
-for i in range(n5):
-    print(arr3[i], end=" ")
+# Insertion Sort----------------------------------------------------------------------------
+def insertionSort(arr):
+    n = len(arr)
+
+    for i in range(1, n):
+        key = arr[i]
+
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+    # Merge Sort Recursive----------------------------------------------------------------------------
+    # def mergeSort(self, arr):
+    #     n = len(arr)
+    #     if n > 1:
+    #         mid = n // 2  # need // for floor division, using / will give error
+    #         left = arr[:mid]  #:mid returns everything before mid including mid
+    #         right = arr[mid:]  # mid: returns everything after mid
+    #
+    #         self.mergeSort(left)
+    #         self.mergeSort(right)
+    #
+    #         i = j = k = 0
+    #
+    #         while i < len(left) and j < len(right):
+    #             if left[i] < right[j]:
+    #                 arr[k] = left[i]
+    #                 i += 1
+    #             else:
+    #                 arr[k] = right[j]
+    #                 j += 1
+    #             k += 1
+    #
+    #         while i < len(left):
+    #             arr[k] = left[i]
+    #             i += 1
+    #             k += 1
+    #
+    #         while j < len(right):
+    #             arr[k] = right[j]
+    #             j += 1
+    #             k += 1
+
+
+# Merge Sort Iterative---------------------------------------------------------------------------------
+def mergeSort(a):
+    current_size = 1
+    # traversing subarrays
+    while current_size < len(a) - 1:
+        left = 0
+        # subarray being sorted
+        while left < len(a) - 1:
+            # calculating mid value
+            mid = left + current_size - 1
+            # current_size
+            right = ((2 * current_size + left - 1, len(a) - 1)[2 * current_size + left - 1 > len(a) - 1])
+            # Merge
+            merge(a, left, mid, right)
+            left = left + current_size * 2
+        # Increasing sub array size
+        current_size = 2 * current_size
+
+
+# Merge Function
+def merge(a, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+    L = [0] * n1
+    R = [0] * n2
+    for i in range(0, n1):
+        L[i] = a[l + i]
+    for i in range(0, n2):
+        R[i] = a[m + i + 1]
+
+    i, j, k = 0, 0, l
+    while i < n1 and j < n2:
+        if L[i] > R[j]:
+            a[k] = R[j]
+            j += 1
+        else:
+            a[k] = L[i]
+            i += 1
+        k += 1
+    while i < n1:
+        a[k] = L[i]
+        i += 1
+        k += 1
+    while j < n2:
+        a[k] = R[j]
+        j += 1
+        k += 1
+
+
+# Quick Sort----------------------------------------------------------------------------
+def quickSort(arr, low, high):
+    if low < high:
+        par = partition(arr, low, high)
+
+        quickSort(arr, low, par - 1)
+        quickSort(arr, par + 1, high)
+
+
+# arr1 = [10, 7, 8, 9, 1, 5]
+# n1 = len(arr1)
+# quickSort(arr1, 0, n1 - 1)
+# print("Sorted array by Quick Sort is:")
+# for i in range(n1):
+#     print(arr1[i], end=" ")
+#
+# arr2 = [4, 10, 9, 3, 6, 1]
+# n2 = len(arr2)
+# bubbleSort(arr2)
+# print("\nSorted array by Bubble Sort is:")
+# for i in range(n2):
+#     print(arr2[i], end=" ")
+#
+# arr3 = [10, 6, 7, 5, 9, 8]
+# n3 = len(arr3)
+# insertionSort(arr3)
+# print("\nSorted array by Insertion Sort is:")
+# for i in range(n3):
+#     print(arr3[i], end=" ")
+#
+# arr4 = [5, 200, 76, 2, 4, 6, 8, 2, 50, 2, 3, 5]
+# n4 = len(arr4)
+# mergeSort(arr4)
+# print("\nSorted array by Merge Sort is:")
+# for i in range(n4):
+#     print(arr4[i], end=" ")
+#
+# arr5 = [5, 10, 7, 5, 6, 1]
+# n5 = len(arr5)
+# selectionSort(arr5)
+# print("\nSorted array by Selection Sort is:")
+# for i in range(n5):
+#     print(arr3[i], end=" ")
+#
+# abc = [5, 4, 3, 2, 1]
+#
+# print(abc)
